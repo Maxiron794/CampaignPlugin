f706b0997d6a2db12471180b436aebe42b508f36
maxiron794 <maksimorischenko213@gmail.com>
2025-06-01 18:40:16 +0200

    init

---
+++ src/stellar/CampaignPlugin.java
@@ -0,0 +1,35 @@
+package stellar;
+
+import arc.*;
+import arc.util.Log;
+import mindustry.game.EventType;
+import mindustry.Vars;
+import mindustry.content.Items;
+import mindustry.content.Planets;
+import mindustry.core.GameState;
+import mindustry.game.Rules;
+import mindustry.game.Team;
+import mindustry.gen.Call;
+import mindustry.mod.*;
+
+public class CampaignPlugin extends Plugin{
+
+    //called when game initializes
+    @Override
+    public void init(){
+        Events.run(EventType.Trigger.update, () -> {
+            if(Vars.state.is(GameState.State.menu)) return;
+            Rules rules = Vars.state.rules;
+            if (rules.planet == Planets.serpulo) {
+                Items.serpuloItems.each(i -> {
+                    Call.setItem(Team.crux.core().tile().build, i, Team.crux.core().tile().block().itemCapacity);
+                });
+            } else if (rules.planet == Planets.erekir) {
+                rules.teams.get(Team.malis).infiniteResources = true;
+                Items.erekirItems.each(i -> {
+                    Call.setItem(Team.malis.core().tile().build, i, Team.malis.core().tile().block().itemCapacity);
+                });
+            }
+        });
+    }
+}
