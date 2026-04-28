ba86846a17a9870b9528d7f24b25303ef5f66158
maxiron794 <maksimorischenko213@gmail.com>
2026-04-28 22:35:32 +0200

    I'm stupid

--- src/stellar/CampaignPlugin.java
+++ src/stellar/CampaignPlugin.java
@@ -1,34 +1,15 @@
 package stellar;
 
-import arc.*;
-import mindustry.game.EventType;
+import arc.Events;
+import mindustry.game.EventType.PlayEvent;
 import mindustry.Vars;
-import mindustry.content.Items;
-import mindustry.content.Planets;
-import mindustry.core.GameState;
-import mindustry.game.Rules;
-import mindustry.game.Team;
-import mindustry.gen.Call;
 import mindustry.mod.*;
 
 public class CampaignPlugin extends Plugin{
-
     @Override
     public void init(){
-        Events.run(EventType.Trigger.update, () -> {
-            if(Vars.state.is(GameState.State.menu)) return;
-            Rules rules = Vars.state.rules;
-            if (rules.planet == Planets.serpulo && Team.crux.core() != null) {
-                rules.teams.get(Team.crux).infiniteResources = true;
-                Items.serpuloItems.each(i -> {
-                    Call.setItem(Team.crux.core().tile.build, i, Team.crux.core().tile.block().itemCapacity);
-                });
-            } else if (rules.planet == Planets.erekir && Team.malis.core() != null) {
-                rules.teams.get(Team.malis).infiniteResources = true;
-                Items.erekirItems.each(i -> {
-                    Call.setItem(Team.malis.core().tile.build, i, Team.malis.core().tile.block().itemCapacity);
-                });
-            }
+        Events.on(PlayEvent.class, event -> {
+            if(Vars.state.rules.waveTeam.core() != null) Vars.state.rules.teams.get(Vars.state.rules.waveTeam).fillItems = true;
         });
     }
 }
